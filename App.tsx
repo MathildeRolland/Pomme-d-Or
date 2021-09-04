6bbc01dabc73069c3d9f7b7282ec450552b2fb3e
Mathilde Rolland <rolland.mmathilde@gmail.com>
2021-09-04 16:06:30 +0200

    Installation of expo-linear-gradient + set up of the background using it

--- App.tsx
+++ App.tsx
@@ -1,16 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text style={styles.text}>Bienvenue sur  
-        <Text style={styles.appName}> Pomme d'Or...</Text>
-      </Text>
-      <Text style={styles.text}>Booste ta concentration tout en créant de nouvelles habitudes!</Text>
-
+      <StatusBar style="auto" backgroundColor="#373737" />
+      <LinearGradient
+        style={styles.container}
+        colors={["#F7F7FF", "#F7F7FF", "#F7B53B"]}
+        start={{x: 0, y: 0}}
+        end={{x: 1, y: 1}}
+      >
+        <Text style={styles.text}>Bienvenue sur  
+          <Text style={styles.appName}> Pomme d'Or...</Text>
+        </Text>
+        <Text style={styles.text}>Booste ta concentration tout en créant de nouvelles habitudes!</Text>
+      </LinearGradient>
     </View>
   );
 }
@@ -18,10 +25,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: '100%',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    textAlign: 'center',
   },
   appName: {
     color: "#F7B53B",
@@ -30,5 +37,5 @@ const styles = StyleSheet.create({
     width: 250,
     textAlign: 'center',
     marginBottom: 30,
-  }
+  },
 });
