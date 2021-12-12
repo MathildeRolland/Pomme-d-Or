f7bcd2c44e6f8fe862c85821553ceb2d0c34929e
Mathilde Rolland <rolland.mmathilde@gmail.com>
2021-12-12 11:48:16 +0100

    added new icon.png + update styles shadow on ios

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -140,6 +140,13 @@ const styles = StyleSheet.create({
         paddingBottom: 20,
         borderRadius: 10,
         elevation: 5,
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
         overflow: 'hidden'
       },
       initTime: {
